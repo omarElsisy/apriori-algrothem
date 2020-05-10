8d9b941aeb33c119b6933a59f73bf275cce0bc30
omarElsisy <ofsisy@gmail.com>
2020-05-10 02:15:42 +0200

    change th dataeframe casting

--- apriori.py
+++ apriori.py
@@ -11,63 +11,79 @@ import pandas as pd
 """the Block1 in code is to adpat the database to aprior algrothem it need to run if you 
 do not have the upgraded_data.csv in your directory """
 """Block 1"""
+    
 df = pd.read_csv("dataset_bigdata.csv") 
-df['Income 75-122.000'] = df['Income 75-122.000'].map({0:'122>income>75', 1:'85>income>75', 2:'95>income>85', 3:'105>income>95', 4:'115>income>105',5:'122>income>115'}) 
-df['Income >123.000']=df['Income >123.000'].map({0:"imcome<123",1:"income is bit bigger than 123",2:"income is bigger than 123",3:"income is much bigger than 123",4:"income is too much bigger than 123"})
-df['Average income']=df['Average income'].map({0:"no income",1:"too bad income",2:"bad income",3:"not so good income",4:"good income",5:"very good income",6:"too good income",7:"high income",8:"too high income",9:"over rated income"})
-df['Purchasing power class']=df['Purchasing power class'].map({0:"under Poverty line",1:"poverty",2:"above poverty line",3:"middle class",4:"above middle class",5:"high class",6:"top high class",7:"golden class",8:"dimamond class",9:"over rated class"})
-df['Contribution private third party insurance']=df['Contribution private third party insurance'].map({0:"f 0",1:"f 1 - 49",2:"f 50 - 99",3:"f 100 - 199",4:"f 200 - 499",5:"f 500 - 999",6:"f 1000 - 4999",7:"f 5000 - 9999",8:"f 10.000 - 19.999",9:"f 20.000 - ?"})
-df['Contribution car policies']=df['Contribution car policies'].map({0:"no Contribution car policies",5:"partial Contribution car policies",6:"full Contribution car policies",7:"Contribution car policies by Limits"})
-df['Contribution fire policies']=df['Contribution fire policies'].map({0:"no Contribution fire policies",1:"The total value of the property:Rs. 50,00,000",2:"Sum Insured Under each Policy: Rs. 50,00,000",3:"Contribution by Limits fire policies",4:"Contribution by Equal Shares fire policies",5:"Proportion of Property covered by insurance: 100%",6:"Total loss payable: [100%] loss i.e. Rs. 500,000",7:"Total Insurers covering the asset: 4"})
-df['AWAPART Number of private third party insurance 1 - 12']=df['AWAPART Number of private third party insurance 1 - 12'].map({0:"no private third party insurance ",1:"1 private third party insurance ",2:"2 private third party insurance ",3:"3 private third party insurance ",4:"4 private third party insurance ",5:"5 private third party insurance ",6:"6 private third party insurance ",7:"7 private third party insurance ",8:"8private third party insurance ",9:"9 private third party insurance ",10:"10 private third party insurance ",11:"11 private third party insurance ",12:"12 private third party insurance "})
-df['Number of car policies']=df['Number of car policies'].map({0:"no car policies",1:"1 car policies",2:"2 car policies",3:"3 car policies",4:"4 car policies",5:"5 car policies"})
-df['Number of fire policies']=df['Number of fire policies'].map({0:"no fire policies",1:"1 fire policies",2:"2 fire policies",3:"3 fire policies",4:"4 fire policies",5:"5 fire policies"})
-df['Customer Subtype']=df['Customer Subtype'].map({
-1:"High Income, expensive child"         
-,2:"Very Important Provincials"
-,3:"High status seniors"
-,4:"Affluent senior apartments"
-,5:"Mixed seniors"
-,6:"Career and childcare"
-,7:"Dinki's (double income no kids)"
-,8:"Middle class families"
-,9:"Modern, complete families"
-,10:"Stable family"
-,11:"Family starters"
-,12:"Affluent young families"
-,13:"Young all american family"
-,14:"Junior cosmopolitan"
-,15:"Senior cosmopolitans"
-,16:"Students in apartments"
-,17:"Fresh masters in the city"
-,18:"Single youth"
-,19:"Suburban youth"
-,20:"Etnically diverse"
-,21:"Young urban have-nots"
-,22:"Mixed apartment dwellers"
-,23:"Young and rising"
-,24:"Young, low educated "
-,25:"Young seniors in the city"
-,26:"Own home elderly"
-,27:"Seniors in apartments"
-,28:"Residential elderly"
-,29:"Porchless seniors: no front yard"
-,30:"Religious elderly singles"
-,31:"Low income catholics"
-,32:"Mixed seniors"
-,33:"Lower class large families"
-,34:"Large family, employed child"
-,35:"Village families"
-,36:"Couples with teens 'Married with children'"
-,37:"Mixed small town dwellers"
-,38:"Traditional families"
-,39:"Large religous families"
-,40:"Large family farms"
-,41:"Mixed rurals"     
-})
-df['Number of houses ']=df['Number of houses '].map({0:"have no houses",1:"have 1 house",2:"have 2 houses",3:"have 3 houses",4:"have 4 houses",5:"have 5 houses",6:"have 6 houses",7:"have 7 houses",8:"have 8 houses",9:"have 9 houses",10:"have 10 houses"})
+df['Income 75-122.000'] ='Income 75-122.000= ' + df['Income 75-122.000'].astype(str)
+df['Income >123.000']='Income >123.000= ' + df['Income >123.000'].astype(str)
+df['Average income']='Average income= '+df['Average income'].astype(str)
+df['Purchasing power class']='Purchasing power class= '+df['Purchasing power class'].astype(str)
+df['Contribution private third party insurance']='Contribution private third party insurance='+df['Contribution private third party insurance'].astype(str)
+df['Contribution car policies']='Contribution car policies= '+df['Contribution car policies'].astype(str)
+df['Contribution fire policies']='Contribution fire policies= '+df['Contribution fire policies'].astype(str)
+df['AWAPART Number of private third party insurance 1 - 12']='AWAPART Number of private third party insurance 1 - 12= '+df['AWAPART Number of private third party insurance 1 - 12'].astype(str)
+df['Number of car policies']='Number of car policies= '+df['Number of car policies'].astype(str)
+df['Number of fire policies']='Number of fire policies= '+df['Number of fire policies'].astype(str)
+df['Customer Subtype']='Customer Subtype= '+df['Customer Subtype'].astype(str)
+df['Number of houses ']='Number of houses= '+df['Number of houses '].astype(str)
+
+
+
+# df['Income 75-122.000'] = df['Income 75-122.000'].map({0:'122>income>75', 1:'85>income>75', 2:'95>income>85', 3:'105>income>95', 4:'115>income>105',5:'122>income>115'}) 
+# df['Income >123.000']=df['Income >123.000'].map({0:"imcome<123",1:"income is bit bigger than 123",2:"income is bigger than 123",3:"income is much bigger than 123",4:"income is too much bigger than 123"})
+# df['Average income']=df['Average income'].map({0:"no income",1:"too bad income",2:"bad income",3:"not so good income",4:"good income",5:"very good income",6:"too good income",7:"high income",8:"too high income",9:"over rated income"})
+# df['Purchasing power class']=df['Purchasing power class'].map({0:"under Poverty line",1:"poverty",2:"above poverty line",3:"middle class",4:"above middle class",5:"high class",6:"top high class",7:"golden class",8:"dimamond class",9:"over rated class"})
+# df['Contribution private third party insurance']=df['Contribution private third party insurance'].map({0:"f 0",1:"f 1 - 49",2:"f 50 - 99",3:"f 100 - 199",4:"f 200 - 499",5:"f 500 - 999",6:"f 1000 - 4999",7:"f 5000 - 9999",8:"f 10.000 - 19.999",9:"f 20.000 - ?"})
+# df['Contribution car policies']=df['Contribution car policies'].map({0:"no Contribution car policies",5:"partial Contribution car policies",6:"full Contribution car policies",7:"Contribution car policies by Limits"})
+# df['Contribution fire policies']=df['Contribution fire policies'].map({0:"no Contribution fire policies",1:"The total value of the property:Rs. 50,00,000",2:"Sum Insured Under each Policy: Rs. 50,00,000",3:"Contribution by Limits fire policies",4:"Contribution by Equal Shares fire policies",5:"Proportion of Property covered by insurance: 100%",6:"Total loss payable: [100%] loss i.e. Rs. 500,000",7:"Total Insurers covering the asset: 4"})
+# df['AWAPART Number of private third party insurance 1 - 12']=df['AWAPART Number of private third party insurance 1 - 12'].map({0:"no private third party insurance ",1:"1 private third party insurance ",2:"2 private third party insurance ",3:"3 private third party insurance ",4:"4 private third party insurance ",5:"5 private third party insurance ",6:"6 private third party insurance ",7:"7 private third party insurance ",8:"8private third party insurance ",9:"9 private third party insurance ",10:"10 private third party insurance ",11:"11 private third party insurance ",12:"12 private third party insurance "})
+# df['Number of car policies']=df['Number of car policies'].map({0:"no car policies",1:"1 car policies",2:"2 car policies",3:"3 car policies",4:"4 car policies",5:"5 car policies"})
+# df['Number of fire policies']=df['Number of fire policies'].map({0:"no fire policies",1:"1 fire policies",2:"2 fire policies",3:"3 fire policies",4:"4 fire policies",5:"5 fire policies"})
+# df['Customer Subtype']=df['Customer Subtype'].map({
+# 1:"High Income, expensive child"         
+# ,2:"Very Important Provincials"
+# ,3:"High status seniors"
+# ,4:"Affluent senior apartments"
+# ,5:"Mixed seniors"
+# ,6:"Career and childcare"
+# ,7:"Dinki's (double income no kids)"
+# ,8:"Middle class families"
+# ,9:"Modern, complete families"
+# ,10:"Stable family"
+# ,11:"Family starters"
+# ,12:"Affluent young families"
+# ,13:"Young all american family"
+# ,14:"Junior cosmopolitan"
+# ,15:"Senior cosmopolitans"
+# ,16:"Students in apartments"
+# ,17:"Fresh masters in the city"
+# ,18:"Single youth"
+# ,19:"Suburban youth"
+# ,20:"Etnically diverse"
+# ,21:"Young urban have-nots"
+# ,22:"Mixed apartment dwellers"
+# ,23:"Young and rising"
+# ,24:"Young, low educated "
+# ,25:"Young seniors in the city"
+# ,26:"Own home elderly"
+# ,27:"Seniors in apartments"
+# ,28:"Residential elderly"
+# ,29:"Porchless seniors: no front yard"
+# ,30:"Religious elderly singles"
+# ,31:"Low income catholics"
+# ,32:"Mixed seniors"
+# ,33:"Lower class large families"
+# ,34:"Large family, employed child"
+# ,35:"Village families"
+# ,36:"Couples with teens 'Married with children'"
+# ,37:"Mixed small town dwellers"
+# ,38:"Traditional families"
+# ,39:"Large religous families"
+# ,40:"Large family farms"
+# ,41:"Mixed rurals"     
+# })
+# df['Number of houses ']=df['Number of houses '].map({0:"have no houses",1:"have 1 house",2:"have 2 houses",3:"have 3 houses",4:"have 4 houses",5:"have 5 houses",6:"have 6 houses",7:"have 7 houses",8:"have 8 houses",9:"have 9 houses",10:"have 10 houses"})
 #df['Avg size household '] = df['Avg size household '].map({0:"no_house",1:'verysmall_House', 2:'small_House', 3:'good_House', 4:'big_House', 5:'villa',6:'villawithgarden'}) 
-df.to_csv(r'/home/omarcc/git_environment/apriori-algrothem/d.csv', index = False)
+df.to_csv(r'/home/omarcc/git_environment/apriori-algrothem/upgraded_data.csv', index = False)
 
 print(df)
 """Block 1 finish """
@@ -107,6 +123,8 @@ def enterSets(groupSet, length):
 def grouping(arr):
     return chain(*[combinations(arr, i + 1) for i, a in enumerate(arr)])
 
+def getSupport(item,noOfReapeated,groupOfData):
+            return float(noOfReapeated[item])/len(groupOfData)
 
 def apriori(dataBase, minSupport, minConfidence):
     noOfReapeated = defaultdict(int)
@@ -125,12 +143,10 @@ def apriori(dataBase, minSupport, minConfidence):
         workingSet = currentCSet
         k = k + 1
 
-    def getSupport(item):
-            return float(noOfReapeated[item])/len(groupOfData)
-
+   
     finalItems = []
     for key, value in allData.items():
-        finalItems.extend([(tuple(item), getSupport(item))
+        finalItems.extend([(tuple(item), getSupport(item,noOfReapeated,groupOfData))
                            for item in value])
 
     Finalrules = []
@@ -139,30 +155,32 @@ def apriori(dataBase, minSupport, minConfidence):
             _subsets = map(frozenset, [x for x in grouping(item)])
             for element in _subsets:
                 #print(element)
-                remain = item.difference(element)
-                if len(remain) > 0:
-                    confidence = getSupport(item)/getSupport(element)
-                    left=confidence/getSupport(remain)
-                    leaverage=getSupport(item)-getSupport(element)*getSupport(remain)
+                rightSide = item.difference(element)
+                if len(rightSide) > 0:
+                    confidence = getSupport(item,noOfReapeated,groupOfData)/getSupport(element,noOfReapeated,groupOfData)
+                    left=confidence/getSupport(rightSide,noOfReapeated,groupOfData)
+                    #print(left)
+                    leaverage=getSupport(item,noOfReapeated,groupOfData)-getSupport(element,noOfReapeated,groupOfData)*getSupport(rightSide,noOfReapeated,groupOfData)
+                    #print(leaverage)
                     if confidence >= minConfidence:
-                        Finalrules.append(((tuple(element), tuple(remain)),
+                        Finalrules.append(((tuple(element), tuple(rightSide)),
                                            confidence,left,leaverage))
 
             # i=0
             # for rule, confidence in sorted(toRetRules, key=operator.itemgetter(1)):
                 # pre, post = rule
-                # remain = item.difference(element)
-                # if len(remain) > 0:
-                #     confidence = getSupport(item)/getSupport(pre)
-                #     left=confidence/getSupport(post)
-                #     leaverage=getSupport(item)-getSupport(post)*getSupport(pre)                                      
+                # rightSide = item.difference(element)
+                # if len(rightSide) > 0:
+                #     confidence = getSupport(item,noOfReapeated,groupOfData)/getSupport(pre,noOfReapeated,groupOfData)
+                #     left=confidence/getSupport(post,noOfReapeated,groupOfData)
+                #     leaverage=getSupport(item,noOfReapeated,groupOfData)-getSupport(post,noOfReapeated,groupOfData)*getSupport(pre,noOfReapeated,groupOfData)                                      
                     # toRetRules[i][3]=left
                     # toRetRules[i][3]=leaverage
                     # print(toRetRules[i])               
-    return finalItems, Finalrules
+    return Finalrules
 
 
-def printResults(items, rules):
+def printResults(rules):
 
     print (" RULES:")
     for rule in rules:
@@ -178,46 +196,40 @@ def printResults(items, rules):
 
 
 def dataFromFile(fname):
-        """Function which reads from the file and yields a generator"""
-        file_iter = open(fname, 'rU')
-        for line in file_iter:
-                line = line.strip().rstrip(',')                         # Remove trailing comma
-                record = frozenset(line.split(','))
-                yield record
+        f = open(fname, 'r')
+        for l in f:
+                l = l.strip().rstrip(',')                         # Remove trailing comma
+                r = l.split(',')
+                yield r
 
 
 if __name__ == "__main__":
 
-    optparser = OptionParser()
-    optparser.add_option('-f', '--inputFile',
+    op= OptionParser()
+    op.add_option('-o', '--inputFile',
                          dest='input',
-                         help='filename containing csv',
                          default=None)
-    optparser.add_option('-s', '--minSupport',
-                         dest='minS',
-                         help='minimum support value',
-                         default=0.15,
+    op.add_option('-s', '--minSupport',
+                         dest='minSupport',
+                         default=0.3,
                          type='float')
-    optparser.add_option('-c', '--minConfidence',
-                         dest='minC',
-                         help='minimum confidence value',
-                         default=0.6,
+    op.add_option('-c', '--minConfidence',
+                         dest='minConfidence',
+                         default=0.4,
                          type='float')
 
-    (options, args) = optparser.parse_args()
+    (options, args) = op.parse_args()
 
-    inFile = None
+    f = None
     if options.input is None:
-            inFile = sys.stdin
+            f = sys.stdin
     elif options.input is not None:
-            inFile = dataFromFile(options.input)
+            f = dataFromFile(options.input)
     else:
-            print ('No dataset filename specified, system with exit\n')
-            sys.exit('System will exit')
+            print ('No filename')
+            sys.exit('exit')
 
-    minSupport = options.minS
-    minConfidence = options.minC
-
-    items, rules = apriori(inFile, minSupport, minConfidence)
-
-    printResults(items, rules)
+    minSupport = options.minSupport
+    minConfidence = options.minConfidence
+    rules = apriori(f, minSupport, minConfidence)
+    printResults(rules)
